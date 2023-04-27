1dc41de18af631d5804e16814c0c06fbc3d29c85
Joao Oliveira <jvmdo.eng16@uea.edu.br>
2023-04-26 20:51:22 -0400

    feat[pages/register/intervals]: add form validation

--- reactjs/ignite-call/src/pages/register/intervals/index.page.tsx
+++ reactjs/ignite-call/src/pages/register/intervals/index.page.tsx
@@ -5,22 +5,63 @@ import { Button, Checkbox, Text, TextInput } from '@ignite-ui/react'
 import { Controller, useFieldArray, useForm, useWatch } from 'react-hook-form'
 import { ArrowRight } from 'phosphor-react'
 import { Formatter } from '@/utils/formatter'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ValidationMessage } from '@/components/ValidationMessage'
+
+const IntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekday: z.number().min(0).max(6),
+        checked: z.boolean(),
+        start: z.string(),
+        end: z.string(),
+      }),
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter(({ checked }) => checked))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Selecione pelo menos um dia da semana.',
+    })
+    .transform((intervals) =>
+      intervals.map((interval) => ({
+        weekday: interval.weekday,
+        startInMinutes: Formatter.calendarTimeToMinutes(interval.start),
+        endInMinutes: Formatter.calendarTimeToMinutes(interval.end),
+      })),
+    )
+    .refine(
+      (intervals) =>
+        intervals.every(
+          ({ startInMinutes, endInMinutes }) => endInMinutes > startInMinutes,
+        ),
+      {
+        message:
+          'O horário de término não pode ser menor ou igual ao horário de início',
+      },
+    ),
+})
+
+type IntervalsFormData = z.input<typeof IntervalsFormSchema>
+type IntervalsFormDataValidated = z.output<typeof IntervalsFormSchema>
 
 export default function Intervals() {
   const {
     control,
     handleSubmit,
     register,
-    formState: { isSubmitting },
-  } = useForm({
+    formState: { isSubmitting, errors },
+  } = useForm<IntervalsFormData>({
+    resolver: zodResolver(IntervalsFormSchema),
     defaultValues: {
       intervals: [
         { weekday: 0, checked: false, start: '08:00', end: '17:00' },
-        { weekday: 1, checked: false, start: '08:00', end: '17:00' },
-        { weekday: 2, checked: false, start: '08:00', end: '17:00' },
-        { weekday: 3, checked: false, start: '08:00', end: '17:00' },
-        { weekday: 4, checked: false, start: '08:00', end: '17:00' },
-        { weekday: 5, checked: false, start: '08:00', end: '17:00' },
+        { weekday: 1, checked: true, start: '08:00', end: '17:00' },
+        { weekday: 2, checked: true, start: '08:00', end: '17:00' },
+        { weekday: 3, checked: true, start: '08:00', end: '17:00' },
+        { weekday: 4, checked: true, start: '08:00', end: '17:00' },
+        { weekday: 5, checked: true, start: '08:00', end: '17:00' },
         { weekday: 6, checked: false, start: '08:00', end: '17:00' },
       ],
     },
@@ -35,8 +76,9 @@ export default function Intervals() {
   })
 
   async function handleOnSubmit(data: any) {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    console.log(data)
+    const validatedData = data as IntervalsFormDataValidated
+    await new Promise((resolve) => setTimeout(resolve, 250))
+    console.log(validatedData)
   }
 
   return (
@@ -73,14 +115,12 @@ export default function Intervals() {
               <div>
                 <TextInput
                   type="time"
-                  step={60}
                   inputMode="numeric"
                   {...register(`intervals.${index}.start` as const)}
                   disabled={!inputs[index].checked}
                 />
                 <TextInput
                   type="time"
-                  step={60}
                   inputMode="numeric"
                   disabled={!inputs[index].checked}
                   {...register(`intervals.${index}.end` as const)}
@@ -89,6 +129,11 @@ export default function Intervals() {
             </IntervalsItem>
           ))}
         </IntervalsInnerBox>
+        <ValidationMessage
+          name="intervals"
+          errors={errors}
+          css={{ marginTop: '-$3' }}
+        />
         <Button type="submit" form="intervals" disabled={isSubmitting}>
           Próximo passo <ArrowRight size={20} weight="bold" />
         </Button>
--- reactjs/ignite-call/src/utils/formatter.ts
+++ reactjs/ignite-call/src/utils/formatter.ts
@@ -3,11 +3,11 @@ export const Formatter = {
    * Formats a number representing a day of the week to its corresponding name
    * of the day of the week in the default language of the user's browser.
    *
-   * @param day - A number between 0 and 6, where 0 is Sunday and 6 is Saturday.
+   * @param {number} day - A number between 0 and 6, where 0 is Sunday and 6 is Saturday.
    *
-   * @returns A string representing the weekday name.
+   * @returns {string} A string representing the weekday name.
    */
-  weekdayName: function (day: number) {
+  weekdayName: function (day: number): string {
     const date = new Date()
     date.setDate(day - date.getDate())
 
@@ -16,5 +16,14 @@ export const Formatter = {
     }).format(date)
 
     return weekdayName.slice(0, 1).toUpperCase().concat(weekdayName.slice(1))
+  },
+  /**
+   * Converts a time string in the format HH:MM to minutes.
+   * @param {string} calendarTime - The time string to convert.
+   * @returns {number} The time in minutes.
+   */
+  calendarTimeToMinutes: function (calendarTime: string): number {
+    const [hours, minutes] = calendarTime.split(':')
+    return Number(hours) * 60 + Number(minutes)
   },
 }
