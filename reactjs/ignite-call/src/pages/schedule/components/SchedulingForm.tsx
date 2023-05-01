decb6bdc2456a0b8b7a08fb46d401801c2abffdd
Joao Oliveira <jvmdo.eng16@uea.edu.br>
2023-04-30 21:44:39 -0400

    feat: [pages/schedule]: add form component

---
+++ reactjs/ignite-call/src/pages/schedule/components/SchedulingForm.tsx
@@ -0,0 +1,171 @@
+import { breakpoints } from '@/styles/globals'
+import {
+  Box,
+  Button,
+  Text,
+  TextArea,
+  TextInput,
+  styled,
+} from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { z } from 'zod'
+import isEmail from 'validator/lib/isEmail'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ValidationMessage } from '@/components/ValidationMessage'
+
+/* 
+  Styles
+*/
+const S_SchedulingForm = styled(Box, {
+  display: 'grid',
+  gap: '$6',
+
+  maxWidth: '33.75rem',
+  padding: '$3',
+
+  [`@media (min-width: ${breakpoints.md})`]: {
+    padding: '$4',
+  },
+
+  [`@media (min-width: ${breakpoints.lg})`]: {
+    width: '33.75rem',
+    padding: '$6',
+  },
+
+  '> hr': {
+    border: 'none',
+    borderBottom: '1px solid $gray600',
+  },
+})
+
+const ScheduleInfo = styled('div', {
+  display: 'flex',
+  gap: '$4',
+
+  '> span': {
+    display: 'inline-flex',
+    gap: '$2',
+    alignItems: 'center',
+
+    [`> ${Text}`]: {
+      color: '$white !important',
+    },
+  },
+})
+
+const Form = styled('form', {
+  display: 'grid',
+  gap: '$6',
+
+  '> div:last-of-type': {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+})
+
+const InputField = styled('div', {
+  display: 'grid',
+  gap: '$2',
+
+  '> label': {
+    color: '$white',
+  },
+})
+
+/* 
+  Component
+*/
+const SchedulingFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Insira pelo menos 3 caracteres' })
+    .regex(/^[a-z-]+$/i, {
+      message: 'Insira apenas letras ou traços (-)',
+    })
+    .transform((value) => value.toLowerCase()),
+  email: z
+    .string()
+    .min(1, {
+      message: 'Insira seu email',
+    })
+    .refine((value) => isEmail(value), {
+      message: 'Formato de email incorreto',
+    }),
+  observations: z.string().nullable(),
+})
+
+type SchedulingFormData = z.infer<typeof SchedulingFormSchema>
+
+export function SchedulingForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SchedulingFormData>({
+    resolver: zodResolver(SchedulingFormSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+    },
+  })
+
+  function handleOnSubmit(data: SchedulingFormData) {
+    console.log(data)
+  }
+
+  function handleCancelSubmit() {
+    console.count('Cancelled!')
+  }
+
+  return (
+    <S_SchedulingForm as="main" onSubmit={handleSubmit(handleOnSubmit)}>
+      <ScheduleInfo>
+        <span>
+          <CalendarBlank size={20} />
+          <Text as="span">30 de Abril de 2023</Text>
+        </span>
+        <span>
+          <Clock size={20} />
+          <Text as="span">20h30</Text>
+        </span>
+      </ScheduleInfo>
+      <hr />
+      <Form>
+        <InputField>
+          <label htmlFor="username">Seu nome de usuário</label>
+          <TextInput
+            id="username"
+            prefix="ignite.com/"
+            size="sm"
+            {...register('username')}
+          />
+          <ValidationMessage
+            name="username"
+            errors={errors}
+            css={{ marginTop: '-$2' }}
+          />
+        </InputField>
+        <InputField>
+          <label htmlFor="email">Endereço de email</label>
+          <TextInput id="email" {...register('email')} />
+          <ValidationMessage
+            name="email"
+            errors={errors}
+            css={{ marginTop: '-$2' }}
+          />
+        </InputField>
+        <InputField>
+          <label htmlFor="observations">Observações</label>
+          <TextArea id="observations" {...register('observations')} />
+        </InputField>
+        <div>
+          <Button variant="tertiary" type="button" onClick={handleCancelSubmit}>
+            Cancelar
+          </Button>
+          <Button>Confirmar</Button>
+        </div>
+      </Form>
+    </S_SchedulingForm>
+  )
+}
