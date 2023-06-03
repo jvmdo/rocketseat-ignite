a642c5e0575952dba8ea3bc27ad75f8038089567
Joao Oliveira <jvmdo.eng16@uea.edu.br>
2023-06-03 16:54:38 -0400

    feat[layouts/BookDrawer]: add book data mutation

--- reactjs/bookwise/src/layouts/MainLayout/components/BookDrawer/index.tsx
+++ reactjs/bookwise/src/layouts/MainLayout/components/BookDrawer/index.tsx
@@ -5,14 +5,37 @@ import { BookDetails } from './components/BookDetails'
 import { BookReviews } from './components/BookReviews'
 import { useContext } from 'react'
 import { MainLayoutContext } from '@/contexts/MainLayoutContext'
+import { api } from '@/lib/axios'
+import { useSession } from 'next-auth/react'
+import { useSWRConfig } from 'swr'
 
 export function BookDrawer() {
   const { drawerBook, setDrawerBook } = useContext(MainLayoutContext)
+  const { data: session, status } = useSession()
+  const { mutate } = useSWRConfig()
 
   const open = Boolean(drawerBook)
+  const userId = session?.user.id
+  const bookId = drawerBook?.id
 
   function setOpen(open: boolean) {
     setDrawerBook(open ? drawerBook : undefined)
+
+    // On drawer close, update last read book and "Lido" tag on Explorer page
+    if (!open && status === 'authenticated') {
+      try {
+        const lastReadKey = `/users/${userId}/last-read`
+        mutate(lastReadKey, () => updater(lastReadKey, bookId), {
+          revalidate: false,
+        })
+
+        // Match keys that starts with `/books` not followed by `/{uuid}`
+        const explorerKeyPattern = /^\/books(?!\/[a-f0-9-]{36})/gi
+        mutate((key) => typeof key === 'string' && explorerKeyPattern.test(key))
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 
   return (
@@ -35,3 +58,10 @@ export function BookDrawer() {
     </div>
   )
 }
+
+async function updater(path: string, bookId?: string) {
+  return (await api.put(path, { bookId })).data
+}
+
+// Complexer regex to match keys used on Explorer page
+// ^\/books(?:\?(?:search=[^&\s]+(?:&tags=[^&\s]+)*|tags=[^&\s]+(?:&tags=[^&\s]+)*))?$
