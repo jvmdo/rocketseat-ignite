1b4e8bcfb1d59307b55ae7b5dd4eb8e4793d1c0c
Joao Oliveira <jvmdo.eng16@uea.edu.br>
2023-06-11 16:14:13 -0400

    perf: sharp image optimizer

--- reactjs/bookwise/prisma/constants/reviews.ts
+++ reactjs/bookwise/prisma/constants/reviews.ts
@@ -16,7 +16,7 @@ export const reviews = [
     description: `Accusantium, ut veniam perferendis pariatur nemo vero, non veritatis praesentium, voluptate ipsam cumque esse a eaque! Harum magni tenetur doloribus. Quisquam cum commodi quam tempora eligendi voluptatem optio delectus aliquam! Tempore eum provident, magni sapiente est soluta unde eveniet labore consequatur? Delectus, voluptate similique? Reprehenderit, eius earum nulla, inventore esse aut, ipsa voluptatibus veritatis impedit magnam temporibus est officia! Ipsam.`,
     book_id: '66cb0f47-7e20-4492-b640-9c020fcae6f2',
     user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60',
-    created_at: '2023-06-11T02:19:15.867Z',
+    created_at: '2023-06-11T02:10:15.867Z',
   },
   {
     id: uuidv4(),
@@ -24,7 +24,7 @@ export const reviews = [
     description: `Minima doloribus, ex ab sint architecto nulla eligendi repudiandae expedita neque sunt asperiores sed omnis excepturi officia incidunt consequuntur voluptas dolore dolores, odio obcaecati quam? Iusto illum pariatur illo enim.`,
     book_id: 'd0590f9a-dd89-42fd-9bbb-bf26c2e4dcf9',
     user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60',
-    created_at: '2023-06-16T02:19:15.867Z',
+    created_at: '2023-06-10T02:19:15.867Z',
   },
   {
     id: uuidv4(),
@@ -40,7 +40,7 @@ export const reviews = [
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum totam maiores provident possimus? Magni exercitationem deleniti facere quaerat, eligendi mollitia enim sequi, quae, quidem totam aliquid ipsa odit ad obcaecati. Amet in aperiam libero repellendus nostrum suscipit nemo, quod explicabo quidem, mollitia, officiis expedita? Eos corporis eum adipisci minus non esse nisi quidem`,
     book_id: 'c8176d86-896a-4c21-9219-6bb28cccaa5f',
     user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60',
-    created_at: '2023-06-20T02:19:15.867Z',
+    created_at: '2023-05-20T02:19:15.867Z',
   },
   {
     id: uuidv4(),
@@ -48,7 +48,7 @@ export const reviews = [
     description: `Recusandae, nam ad earum sequi quae qui nihil exercitationem ipsum in temporibus. Possimus, ipsum maiores sapiente est porro consectetur quibusdam omnis iusto, consequatur perspiciatis incidunt mollitia dolorum autem quaerat placeat! Perspiciatis libero quidem perferendis quasi itaque labore provident, deserunt, culpa ut officia suscipit sapiente magnam. Voluptate esse suscipit, eaque enim cupiditate nam, hic fugiat adipisci eum possimus animi tenetur eius.`,
     book_id: 'd2870ad0-3312-4ac2-af9f-76af6565587d',
     user_id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60',
-    created_at: '2023-06-15T02:19:15.867Z',
+    created_at: '2023-06-10T02:19:15.867Z',
   },
   {
     id: uuidv4(),
@@ -72,7 +72,7 @@ export const reviews = [
     description: `Ea, distinctio perferendis enim saepe suscipit necessitatibus aliquam illum iure blanditiis. Aperiam temporibus voluptate dignissimos cum ipsa maiores harum laudantium, officiis animi a accusantium repellendus molestiae, nemo alias sit quis. Inventore rem cum deleniti quos, nihil illum, aliquam nesciunt consectetur totam odio explicabo. Minima nihil laudantium tenetur voluptates dicta blanditiis quae aliquid facere, distinctio itaque voluptas suscipit, quos earum ut`,
     book_id: '4fd2b389-b211-40b5-9797-f78cbb985645',
     user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502',
-    created_at: '2023-06-13T02:19:15.867Z',
+    created_at: '2023-06-11T02:13:15.867Z',
   },
   {
     id: uuidv4(),
@@ -88,7 +88,7 @@ export const reviews = [
     description: 'Nec tempor nunc in egestas.',
     book_id: '0440ad7d-230e-4573-b455-84ca38b5d339',
     user_id: 'c296c6c0-5c59-40dd-aa8a-ef2b015b7502',
-    created_at: '2023-06-19T02:19:15.867Z',
+    created_at: '2023-05-19T02:19:15.867Z',
   },
   {
     id: uuidv4(),
@@ -96,7 +96,7 @@ export const reviews = [
     description: `Semper et sapien proin vitae nisi. Feugiat neque integer donec et aenean posuere amet ultrices. Cras fermentum id pulvinar varius leo a in. Amet libero pharetra nunc elementum fringilla velit ipsum. Sed vulputate massa velit nibh`,
     book_id: 'e688c24f-d14d-4607-a12e-90e6e367398d',
     user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef',
-    created_at: '2023-06-11T02:19:15.867Z',
+    created_at: '2023-06-11T02:14:15.867Z',
   },
 
   {
@@ -105,7 +105,7 @@ export const reviews = [
     description: `Distinctio numquam accusamus consectetur adipisci voluptatibus architecto reiciendis, commodi explicabo labore nisi, dolorem repudiandae pariatur fuga obcaecati. Explicabo, aperiam aut, fugit, quisquam ex laudantium culpa quidem tempora ratione necessitatibus dolorum? Fuga quaerat distinctio deserunt? Unde alias quisquam qui dolore, veniam atque nam eius cupiditate saepe sunt sapiente dolores esse sint cumque animi nemo amet. Modi accusantium quos minus ipsum nostrum.`,
     book_id: '375948a7-bca3-4b59-9f97-bfcde036b4ca',
     user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef',
-    created_at: '2023-06-11T02:19:15.867Z',
+    created_at: '2023-06-11T02:15:15.867Z',
   },
 
   {
@@ -114,7 +114,7 @@ export const reviews = [
     description: `Eaque voluptatum nisi dignissimos maiores magni laboriosam non ut quo? Eum totam expedita natus eveniet rem sequi vel officiis perspiciatis aperiam excepturi inventore libero magni labore reiciendis, aut ex repudiandae.`,
     book_id: '14f410df-b28a-4e72-b1b4-363e26e160dd',
     user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef',
-    created_at: '2023-06-12T02:19:15.867Z',
+    created_at: '2023-06-02T02:19:15.867Z',
   },
 
   {
@@ -123,7 +123,7 @@ export const reviews = [
     description: `Reiciendis reprehenderit laudantium, ullam error molestias neque, incidunt quos praesentium dolorum aspernatur sequi iste ut repellat eius? In fugiat, voluptate minima repudiandae error ut recusandae maxime nesciunt reiciendis exercitationem ex. Nihil ab soluta minus reiciendis maiores, ut excepturi, modi tempore, placeat tenetur consequuntur? Odit alias eos nobis facilis culpa, repudiandae doloribus totam consectetur molestias a voluptatum! Distinctio architecto repellendus voluptatem.`,
     book_id: '6de9f6b8-5ff4-4e06-b9f4-843eca462803',
     user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef',
-    created_at: '2023-06-15T02:19:15.867Z',
+    created_at: '2023-06-10T02:19:15.867Z',
   },
 
   {
@@ -132,6 +132,6 @@ export const reviews = [
     description: `Facilis similique natus, cupiditate non, aut corrupti rerum dolorem praesentium sit aspernatur magnam magni unde veniam et ad harum, itaque tempora. Corrupti optio tenetur eligendi eos quos deleniti exercitationem quae! Explicabo, labore necessitatibus fugit pariatur magnam reiciendis earum. Et veritatis explicabo asperiores cumque, eius dolore impedit aut tempora rerum quaerat delectus dolorum vitae iusto illum temporibus saepe mollitia deleniti amet`,
     book_id: '404e47f8-da53-44fd-ab53-37ed171c3a9f',
     user_id: '4383f783-6ce1-4f92-b1dd-7a7a693c4aef',
-    created_at: '2023-06-18T02:19:15.867Z',
+    created_at: '2023-06-10T02:19:15.867Z',
   },
 ]
