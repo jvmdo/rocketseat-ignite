045560301a29abc1f26d56570ea4ea855d135063
jvmdo <deoliveira.jvm@gmail.com>
2023-07-30 19:42:48 -0400

    tests: add POST /sign-up

--- nodejs/daily-diet/src/test/authRoutes.test.ts
+++ nodejs/daily-diet/src/test/authRoutes.test.ts
@@ -16,4 +16,37 @@ describe('Authentication routes', () => {
     execSync('npm run knex -- migrate:rollback --all')
     execSync('npm run knex -- migrate:latest')
   })
+
+  describe('POST /sign-up', () => {
+    it.skip('should create a new user', async () => {
+      const credentials = {
+        name: 'Saul Goodman',
+        username: 'itsallgoodman',
+        password: '123123',
+      }
+
+      await request(app.server).post(`/sign-up`).send(credentials).expect(201)
+    })
+
+    it.skip('should reject creating two accounts with the same username', async () => {
+      const credentials = {
+        name: 'Saul Goodman',
+        username: 'itsallgoodman',
+        password: '123123',
+      }
+
+      await request(app.server).post(`/sign-up`).send(credentials)
+      await request(app.server).post(`/sign-up`).send(credentials).expect(409)
+    })
+
+    it('should reject creating an account with wrong credentials format', async () => {
+      const credentials = {
+        name: 'Saul Goodman',
+        username: 'itsallgoodman',
+        password: 123123, // should be string
+      }
+
+      await request(app.server).post(`/sign-up`).send(credentials).expect(400)
+    })
+  })
 })
