a1d9971409bbed8123fcc576416402cb62838bcb
jvmdo <deoliveira.jvm@gmail.com>
2023-08-03 21:07:39 -0400

    test: sign up use case

---
+++ nodejs/gympass-solid/src/repository/in-memory/users.ts
@@ -0,0 +1,25 @@
+import { Prisma, User } from '@prisma/client'
+import { IUsersRepository } from '../users-repository'
+import { randomUUID } from 'crypto'
+
+export class UsersRepository implements IUsersRepository {
+  private users: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      ...data,
+      id: randomUUID(),
+      createdAt: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email)
+
+    return user ?? null
+  }
+}
---
+++ nodejs/gympass-solid/src/usecases/auth/sign-up.test.ts
@@ -0,0 +1,69 @@
+import { UsersRepository } from '@/repository/in-memory/users'
+import { describe, expect, it } from 'vitest'
+import { SignUpUseCase } from './sign-up'
+import bcrypt from 'bcryptjs'
+import { UserAlreadyExistsError } from '../errors/user-already-exists'
+
+describe('Sign up use case', () => {
+  it('should create a new user', async () => {
+    const usersRepository = new UsersRepository()
+    const signUpUseCase = new SignUpUseCase(usersRepository)
+
+    const data = {
+      name: 'Jane Doe',
+      email: 'jane_doe@example.com',
+      password: 'jane123',
+    }
+
+    const user = await signUpUseCase.execute(data)
+
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+
+    expect(user).toHaveProperty('id')
+    expect(user.id).toMatch(uuidRegex)
+  })
+
+  it('should hash the provided password', async () => {
+    const usersRepository = new UsersRepository()
+    const signUpUseCase = new SignUpUseCase(usersRepository)
+
+    const data = {
+      name: 'Jane Doe',
+      email: 'jane_doe@example.com',
+      password: 'jane123',
+    }
+
+    const user = await signUpUseCase.execute(data)
+
+    const isPasswordCorrectlyHashed = await bcrypt.compare(
+      data.password,
+      user.passwordHash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should throw on duplicated emails', async () => {
+    const usersRepository = new UsersRepository()
+    const signUpUseCase = new SignUpUseCase(usersRepository)
+
+    const data = {
+      name: 'Jane Doe',
+      email: 'jane_doe@example.com',
+      password: 'jane123',
+    }
+
+    await signUpUseCase.execute(data)
+
+    await expect(signUpUseCase.execute(data)).rejects.toBeInstanceOf(
+      UserAlreadyExistsError,
+    )
+
+    try {
+      await signUpUseCase.execute(data)
+    } catch (error) {
+      expect(error).toHaveProperty('code', 409)
+    }
+  })
+})
